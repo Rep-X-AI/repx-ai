e935266c7796cf0c7e73b1d761a76efae052b42c
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-03-23 19:25:53 +0530

    fix errors

--- src/Components/InstallButton.jsx
+++ src/Components/InstallButton.jsx
@@ -1,27 +1,40 @@
-// eslint-disable-next-line
-
 import React from 'react'
-
-// eslint-disable-next-line
-import usePwa from "use-pwa";
-
+import { useCallback } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { usePwa } from '@dotmind/react-use-pwa';
+
 
 const InstallButton = () => {
-  
-  const { appinstalled, canInstallprompt, isLoading, showInstallPrompt } =
-    usePwa();
-  
-  if (!canInstallprompt || appinstalled || isLoading) return null;
+  const {
+    installPrompt,
+    isInstalled,
+    isStandalone,
+    isOffline,
+    canInstall,
+  } = usePwa();
 
-  return (
-    
-    <button onClick={showInstallPrompt}
-      className="hero-button-gradient inline-flex gap-2 rounded-lg py-3 px-7 text-white font-medium ease-in duration-300 hover:opacity-80">
-      Install <FontAwesomeIcon icon={faDownload} className='pt-1' />
-    </button>
-  )
-}
+  const handleInstallPrompt = useCallback(() => {
+    if (canInstall) {
+      installPrompt();
+    }
+  }, [canInstall, installPrompt]);
 
-export default InstallButton
+  if (isOffline) {
+    return <p>Please check your network 📶</p>;
+  }
+
+  if (!canInstall || isInstalled) {
+    return null;
+  }
+
+    if (!isInstalled || !isStandalone) {
+      return (
+        <button onClick={handleInstallPrompt} className={`hero-button-gradient inline-flex gap-2 rounded-lg py-3 px-7 text-white font-medium ease-in duration-300 hover:opacity-80 ${isInstalled ? "hidden" : ""}`}>
+          Install <FontAwesomeIcon icon={faDownload} className='pt-1' />
+        </button>
+      );
+    }
+  }
+
+  export default InstallButton
