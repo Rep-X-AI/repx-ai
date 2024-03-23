edde8053a1d5ebe38c40c8e04843989109517b1b
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-03-23 19:51:38 +0530

    fix app btn

--- src/Components/InstallButton.jsx
+++ src/Components/InstallButton.jsx
@@ -2,21 +2,48 @@ import React from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { usePwa } from '@dotmind/react-use-pwa';
+import { useState, useEffect } from 'react';
 
 
 const InstallButton = () => {
   const {
-    installPrompt,
     isInstalled,
-    canInstall,
   } = usePwa();
 
-  if (!canInstall || isInstalled) {
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
+
+  useEffect(() => {
+    const handler = e => {
+      e.preventDefault();
+      console.log("we are being triggered :D");
+      setSupportsPWA(true);
+      setPromptInstall(e);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+
+    return () => window.removeEventListener("transitionend", handler);
+  }, []);
+
+  const onClick = evt => {
+    evt.preventDefault();
+    if (!promptInstall) {
+      return;
+    }
+    promptInstall.prompt();
+  };
+  if (!supportsPWA) {
+    return null;
+  }
+
+  if (isInstalled) {
     return null;
   }
 
   return (
-    <button onClick={installPrompt} className={`hero-button-gradient inline-flex gap-2 rounded-lg py-3 px-7 text-white font-medium ease-in duration-300 hover:opacity-80 ${isInstalled ? "hidden" : ""}`}>
+    <button onClick={onClick} id="setup_button"
+      aria-label="Install app"
+      title="Install app" className={`hero-button-gradient inline-flex gap-2 rounded-lg py-3 px-7 text-white font-medium ease-in duration-300 hover:opacity-80 ${isInstalled ? "hidden" : ""}`}>
       Install <FontAwesomeIcon icon={faDownload} className='pt-1' />
     </button>
   );
