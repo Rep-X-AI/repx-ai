16abaefee9531af4edf0e82983a12842bc9f3f32
Subhadeep Roy <subha9.5roy350@gmail.com>
2024-03-23 19:38:57 +0530

    feat: fix installbtn

--- src/Components/InstallButton.jsx
+++ src/Components/InstallButton.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useCallback } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
 import { usePwa } from '@dotmind/react-use-pwa';
@@ -9,32 +8,18 @@ const InstallButton = () => {
   const {
     installPrompt,
     isInstalled,
-    isStandalone,
-    isOffline,
     canInstall,
   } = usePwa();
-
-  const handleInstallPrompt = useCallback(() => {
-    if (canInstall) {
-      installPrompt();
-    }
-  }, [canInstall, installPrompt]);
-
-  if (isOffline) {
-    return <p>Please check your network 📶</p>;
-  }
 
   if (!canInstall || isInstalled) {
     return null;
   }
 
-    if (!isInstalled || !isStandalone) {
-      return (
-        <button onClick={handleInstallPrompt} className={`hero-button-gradient inline-flex gap-2 rounded-lg py-3 px-7 text-white font-medium ease-in duration-300 hover:opacity-80 ${isInstalled ? "hidden" : ""}`}>
-          Install <FontAwesomeIcon icon={faDownload} className='pt-1' />
-        </button>
-      );
-    }
-  }
+  return (
+    <button onClick={installPrompt} className={`hero-button-gradient inline-flex gap-2 rounded-lg py-3 px-7 text-white font-medium ease-in duration-300 hover:opacity-80 ${isInstalled ? "hidden" : ""}`}>
+      Install <FontAwesomeIcon icon={faDownload} className='pt-1' />
+    </button>
+  );
+}
 
-  export default InstallButton
+export default InstallButton
