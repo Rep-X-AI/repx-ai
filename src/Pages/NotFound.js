54664f6fc3423e547bb521082a14a924f9c1cb3e
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-03-25 21:01:08 +0530

    Update NotFound.js

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import img from '../Images/matrix.webp';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/");
+  };
+
   return (
-    <div className="container mx-auto mt-10 text-center">
-      <h1 className="text-4xl font-bold text-red-600 mb-4">404 - Page Not Found</h1>
-      <p className="text-lg text-gray-700">The page you are looking for does not exist.</p>
+    <div className="flex flex-col justify-center items-center h-screen bg-black" style={{backgroundImage: `url(${img})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat',}}>
+      <p className="text-white font-bold text-2xl sm:text-5xl" style={{fontSize:"250px"}}>404</p>
+      <h1 className="text-white mb-6 text-3xl font-bold sm:text-5xl xl:text-5xl"> Page Not Found</h1>
+      <button type="button" className="hero-button-gradient inline-flex rounded-lg py-3 px-7 my-3 text-white font-medium ease-in duration-300 hover:opacity-80" onClick={handleNavigate}>Back To Home</button>
     </div>
   );
 };
