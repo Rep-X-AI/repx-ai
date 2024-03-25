60ffe92a555938bc1d99a6fb2323204e38094bc8
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-03-25 11:32:20 +0530

    Create NotFound.js

---
+++ src/Pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="container mx-auto mt-10 text-center">
+      <h1 className="text-4xl font-bold text-red-600 mb-4">404 - Page Not Found</h1>
+      <p className="text-lg text-gray-700">The page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
