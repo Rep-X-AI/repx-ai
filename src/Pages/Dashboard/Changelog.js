635b8ede3e8122d801ae754b10672a9bff311ea0
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2024-04-06 16:33:41 +0530

    Update Changelog.js

--- src/Pages/Dashboard/Changelog.js
+++ src/Pages/Dashboard/Changelog.js
@@ -3,24 +3,24 @@ import React from "react";
 const ChangelogPage = () => {
   return (
     <>
-      <div class="px-4 pt-10 pb-16 sm:px-6 md:px-8">
+      <div className="px-4 pt-10 pb-16 sm:px-6 md:px-8">
       <h1 className="text-white mb-6 text-3xl font-bold sm:text-5xl xl:text-5xl  p-2">Changelog</h1>
       <hr />
-        <div class="mb-2 space-y-5 text-left text-lg">
+        <div className="mb-2 space-y-5 text-left text-lg">
           <p>Check out whats new on Javaistic.</p>
         </div>
-        <section class="text-gray-600 ">
-          <div class="container mx-auto py-12">
-            <div class="flex flex-col items-start rounded-xl  sm:items-center">
-              <ol class="relative w-full border-l border-gray-200 dark:border-gray-700">
-                <li class="mb-10 ml-6">
-                  <span class="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
+        <section className="text-gray-600 ">
+          <div className="container mx-auto py-12">
+            <div className="flex flex-col items-start rounded-xl  sm:items-center">
+              <ol className="relative w-full border-l border-gray-200 dark:border-gray-700">
+                <li className="mb-10 ml-6">
+                  <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                       aria-hidden="true"
-                      class="h-3 w-3 fill-current text-blue-600 dark:text-blue-400"
+                      className="h-3 w-3 fill-current text-blue-600 dark:text-blue-400"
                     >
                       <path
                         fill-rule="evenodd"
@@ -29,29 +29,29 @@ const ChangelogPage = () => {
                       ></path>
                     </svg>
                   </span>
-                  <h3 class="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
+                  <h3 className="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
                     Repx AI v1.7.0
-                    <span class="mr-2 ml-3 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 dark:bg-blue-200 dark:text-blue-800">
+                    <span className="mr-2 ml-3 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 dark:bg-blue-200 dark:text-blue-800">
                       Latest
                     </span>
                   </h3>
-                  <time class="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  <time className="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                     Released on 6 April , 2024
                   </time>
-                  <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                  <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                    Minor Fixes .Some bugs resolved.
                   </p>
                 </li>
               </ol>
-              <ol class="relative w-full border-l border-gray-200 dark:border-gray-700">
-                <li class="mb-10 ml-6">
-                  <span class="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
+              <ol className="relative w-full border-l border-gray-200 dark:border-gray-700">
+                <li className="mb-10 ml-6">
+                  <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                       aria-hidden="true"
-                      class="h-3 w-3 fill-current text-blue-600 dark:text-blue-400"
+                      className="h-3 w-3 fill-current text-blue-600 dark:text-blue-400"
                     >
                       <path
                         fill-rule="evenodd"
@@ -60,24 +60,24 @@ const ChangelogPage = () => {
                       ></path>
                     </svg>
                   </span>
-                  <h3 class="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
+                  <h3 className="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
                     Repx AI v1.6.0
                   </h3>
-                  <time class="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  <time className="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                     Released on 1 April, 2024
                   </time>
-                  <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                  <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                     packed with even more powerful features. Now, our smart classroom goes beyond text-based assignments with cutting-edge image processing capabilities. Equipped with advanced algorithms, it can analyze and process diagrams and equations, providing students and teachers with enhanced tools for visual learning and assessment.
                   </p>
                 </li>
-                <li class="mb-10 ml-6">
-                  <span class="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
+                <li className="mb-10 ml-6">
+                  <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                       aria-hidden="true"
-                      class="h-3 w-3 fill-current text-blue-600 dark:text-blue-400"
+                      className="h-3 w-3 fill-current text-blue-600 dark:text-blue-400"
                     >
                       <path
                         fill-rule="evenodd"
@@ -86,24 +86,24 @@ const ChangelogPage = () => {
                       ></path>
                     </svg>
                   </span>
-                  <h3 class="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
+                  <h3 className="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
                   Repx AI  v1.5.0
                   </h3>
-                  <time class="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  <time className="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                     Released on 21 March ,2024
                   </time>
-                  <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                  <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                     Updated with advanced AI features. Now, our smart classroom utilizes cutting-edge Natural Language Processing (NLP) technology to automatically grade student reports. This innovative feature significantly reduces teachers' workload by efficiently grading assignments, allowing them to focus more on personalized instruction and student development.
                   </p>
                 </li>
-                <li class="mb-10 ml-6">
-                  <span class="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
+                <li className="mb-10 ml-6">
+                  <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                       aria-hidden="true"
-                      class="h-3 w-3 fill-current text-blue-600 dark:text-blue-400"
+                      className="h-3 w-3 fill-current text-blue-600 dark:text-blue-400"
                     >
                       <path
                         fill-rule="evenodd"
@@ -112,24 +112,24 @@ const ChangelogPage = () => {
                       ></path>
                     </svg>
                   </span>
-                  <h3 class="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
+                  <h3 className="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
                   Repx AI  v1.4.0
                   </h3>
-                  <time class="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  <time className="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                     Released on 8 March, 2024
                   </time>
-                  <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                  <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                     Security and Privacy Updates . With enhanced security measures, including robust encryption and advanced cookie management, our smart classroom ensures the utmost protection of your data.
                   </p>
                 </li>
-                <li class="mb-10 ml-6">
-                  <span class="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
+                <li className="mb-10 ml-6">
+                  <span className="absolute -left-3 flex h-6 w-6 items-center justify-center rounded-full bg-blue-200 ring-8 ring-white dark:bg-blue-900 dark:ring-gray-900">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                       aria-hidden="true"
-                      class="h-3 w-3 fill-current text-blue-600 dark:text-blue-400"
+                      className="h-3 w-3 fill-current text-blue-600 dark:text-blue-400"
                     >
                       <path
                         fill-rule="evenodd"
@@ -138,13 +138,13 @@ const ChangelogPage = () => {
                       ></path>
                     </svg>
                   </span>
-                  <h3 class="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
+                  <h3 className="mb-1 flex items-center text-lg font-semibold text-gray-900 dark:text-white">
                     Repx AI V1.0.0
                   </h3>
-                  <time class="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+                  <time className="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                     Released on 1 March ,2024
                   </time>
-                  <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+                  <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                     A smart i Classroom  easing out the workload of both Students and Teachers . Interactive dashboard , clean UI ,Eficient Handling of Assignments,and seemless navigation . Experience a transformative learning environment with Repx AI.
                   </p>
                 </li>
