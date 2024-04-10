e5c5c34432cf4cf173135a34de59d561f621633e
ArunavaCoderEm <acecankill@gmail.com>
2024-04-10 23:20:45 +0530

    join assignment style

--- src/Pages/Dashboard/JoinAssignment.js
+++ src/Pages/Dashboard/JoinAssignment.js
@@ -36,24 +36,31 @@ export default function JoinAssignment() {
   }
 
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="w-full h-96 max-w-3xl bg-transparent p-6 rounded-lg shadow-lg shadow-violet-950 shadow-2xl ">
-        <h1 className="text-center text-3xl md:text-4xl lg:text-5xl font-bold leading-none mb-2 text-transparent bg-gradient-to-bl from-purple-300 via-pink-300 to-purple-500 bg-clip-text p-4 m-6">
-          Join Assignment
-        </h1>
+    
+    <div className="w-full h-screen  justify-center items-center">
+      <h1 className="text-3xl mb-1 text-left font-bold leading-none md:text-4xl lg:text-5xl pb-2 bg-gradient-to-bl from-purple-300 via-pink-300 to-purple-500 bg-clip-text text-transparent">
+        {" "}
+        Join Assignment{" "}
+      </h1>
+      <p className="mb-3 mt-2 text-lg font-normal text-purple-300 lg:text-xl text-left">
+        Fill in the student code here to access the assignment.
+      </p>
+      <hr className="h-px my-8 mt-1 bg-gray-700 border-0" />
+
+      <div className="w-full overflow-hidden h-96 max-w-3xl bg-transparent m-auto p-6 rounded-lg">
         <form className="mt-4" onSubmit={handleSubmit}>
-          <p className="text-lg font-semibold text-transparent bg-gradient-to-bl from-purple-300 via-pink-300 to-purple-500 bg-clip-text py-2">
-            Enter Assignment Code
+          <p className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white text-center">
+            Enter class code provided by teacher.
           </p>
           <input
-            className="w-full bg-gradient-to-b from-purple-100 to-purple-300 text-black rounded-lg outline-none p-6 border-2 border-[#030017] placeholder-gray-700 focus:ring-1 focus:ring-purple-400 focus:ring-offset-1 focus:ring-offset-transparent"
+            className="w-full bg-gradient-to-b from-purple-100 to-purple-300 text-black rounded-lg outline-none p-4 border-2 placeholder-gray-700 focus:ring-1 focus:ring-purple-400 focus:ring-offset-1 focus:ring-offset-transparent mb-1"
             type="text"
             placeholder="Assignment Code"
             value={code}
             onChange={handleChange}
           />
-          <button type="submit" className="mt-4 w-full bg-gradient-to-r from-purple-400 via-pink-500 to-purple-600 text-white text-xl font-semibold p-6  rounded-lg shadow-md hover:from-purple-600 hover:to-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-400 ">
-            Join
+          <button type="submit" className="hero-button-gradient mt-3 rounded-lg py-4 px-7 text-white font-medium tracking-wide transition-all duration-300 ease-in-out hover:opacity-80 hover:scale-95 m-auto w-full">
+            Join Assignment
           </button>
         </form>
       </div>
