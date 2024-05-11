56f458ff0c0df2595d6c50d5d24a35e4d8f7d5e9
ArunavaCoderEm <acecankill@gmail.com>
2024-05-12 00:43:30 +0530

    please work please please

--- src/Pages/Dashboard/Assignment.js
+++ src/Pages/Dashboard/Assignment.js
@@ -324,7 +324,7 @@ export default function Assignment({ role }) {
     }
     console.log(dataa);
     try {
-    const response = await axios.post(`http://35.193.123.233/deleteAssignment`, dataa);//${webUrl}
+    const response = await axios.post(`/deleteAssignment`, dataa);//${webUrl}
       console.log(response.data);
     } catch (error) {
       console.error('Error:', error.response ? error.response.data : error.message);
